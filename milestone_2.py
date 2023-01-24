f50595c4ffe7788480d45757a91abd1a434cf086
Roo2409 <roobikapoor@hotmail.com>
2023-01-24 11:57:07 +0000

    this is the task 2 from milestone2 hangman

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,11 @@
+import random
+word = ['banana','mango','apple','berry','orange']
+print(random.choice(word))
+
+import random
+word = ['banana','mango','berry','orange']
+print(random.choice(word))
+
+
 word_list = ['banana','mango','apple','berry','orange']
 print(word_list)
