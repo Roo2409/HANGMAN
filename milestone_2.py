a6ba15d161caec11e85f45f08a810c5da0760cb7
Roo2409 <roobikapoor@hotmail.com>
2023-01-24 12:56:02 +0000

    This is the task 2

--- milestone_2.py
+++ milestone_2.py
@@ -1,10 +1,8 @@
 import random
-word = ['banana','mango','apple','berry','orange']
-print(random.choice(word))
+word_list = ['banana','mango','apple','berry','orange']
+word = random.choice(word_list)
+print(word)
 
-import random
-word = ['banana','mango','berry','orange']
-print(random.choice(word))
 
 
 word_list = ['banana','mango','apple','berry','orange']
