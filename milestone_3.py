889ae63e2a1706b441492c780c87e15624697fba
Roo2409 <roobikapoor@hotmail.com>
2023-01-25 16:52:51 +0000

    This is trial task

--- milestone_3.py
+++ milestone_3.py
@@ -1,15 +1,21 @@
-word = "apple"
-while True:
-    guess = input("enter a letter").strip()
-    if len(guess)==1 and guess.isalpha():
-
-        if guess in word:
-            print(f"Good guess! {guess} is in the word.")
-        else:
-            print (f"Sorry, {guess} is not in the word. Try again.")
-            
-    else: 
+def check_guess(guess):
+    guess= guess.lower()
+    word = "apple"
+    if guess in word: 
+         print(f"Good guess! {guess} is in the word.")
+    
+    else:
+         print (f"Sorry, {guess} is not in the word. Try again.") 
+#check_guess("a")
+def ask_for_input():
+    while True:
+     guess = input("enter a letter").strip()
+     if len(guess)==1 and guess.isalpha():
+         break
+     
+     else: 
         print("Invalid letter. Please, enter a single alphabetical character.")
         break
 
-    
+    check_guess(guess)
+ask_for_input()
