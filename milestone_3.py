89db93abd1c2730f549f7c07481250b4db98a152
Roo2409 <roobikapoor@hotmail.com>
2023-01-25 12:34:03 +0000

    This is task2 in milestone3 Hangman

--- milestone_3.py
+++ milestone_3.py
@@ -1,9 +1,9 @@
-secretword = "apple"
+word = "apple"
 while True:
     guess = input("enter a letter").strip()
     if len(guess)==1 and guess.isalpha():
 
-        if guess in secretword:
+        if guess in word:
             print(f"Good guess! {guess} is in the word.")
         else:
             print (f"Sorry, {guess} is not in the word. Try again.")
