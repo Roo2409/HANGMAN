05dea5f120bf19f6c577fc77981c72a612ae32df
Roo2409 <roobikapoor@hotmail.com>
2023-01-30 16:38:10 +0000

    change the parameter right

--- milestone_4.py
+++ milestone_4.py
@@ -1,15 +1,15 @@
 import random
 class Hangman:
- def __init__(self,word_list,num_of_lives = 5):
-    self.num_of_lives = int(5)
-    print (num_of_lives)
+ def __init__(self,word_list,num_lives = 5):
+    self.num_lives = num_lives
+    print (num_lives)
     self.word = random.choice(word_list)
     word = self.word
     print(word)
     self.word_guessed = word_guessed
     guess = input("enter the letter")
-    if guess in word:
-      print(guess,)
+    # if guess in word:
+    #   print(guess,)
 
     # word_guessed = "_"
     # while True:
