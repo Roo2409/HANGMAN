ef795333e91071326f01bc1aed843f9f9c5cd8af
Roo2409 <roobikapoor@hotmail.com>
2023-01-30 16:56:04 +0000

    initialised the attributes

--- milestone_4.py
+++ milestone_4.py
@@ -2,12 +2,10 @@ import random
 class Hangman:
  def __init__(self,word_list,num_lives = 5):
     self.num_lives = num_lives
-    print (num_lives)
     self.word = random.choice(word_list)
-    word = self.word
-    print(word)
-    self.word_guessed = word_guessed
-    guess = input("enter the letter")
+    self.word_guessed = ["_"]*len(self.word)
+    
+ 
     # if guess in word:
     #   print(guess,)
 
@@ -21,6 +19,7 @@ class Hangman:
  
 
 word_list = ['banana','mango','apple','berry','orange'] 
-word_guessed = ['_', '_', '_', '_', '_']
-Task1 = hangman(word_list)
-print(Task1)
+word_guessed = ['_']
+Task1 = Hangman(word_list)
+print(Task1.word)
+print(Task1.word_guessed)
