0d90c8daec34324bce0b5d646df978a545bc4427
Roo2409 <roobikapoor@hotmail.com>
2023-01-30 23:10:20 +0000

    trying to call the function but giving an error

--- milestone_4.py
+++ milestone_4.py
@@ -8,17 +8,28 @@ class Hangman:
     self.num_letters = len(set(self.word))
     self.list_of_guesses = [ ]
 
- def check_guess(guess):
-   word = "apple"
-   guess = guess.lower()
-   if guess in word:
+ def check_guess(self,guess):
+   self.guess = guess.lower()
+   if self.guess in self.word:
      print("Good guess! {guess} is in the word.")
+ def ask_for_input():
+   while True:
+     guess = input("enter a letter")
+     for letter in guess:
+       if len(guess)!= 1 and not guess.isalpha():
+         print("Invalid letter. Please, enter a single alphabetical character.")
+       elif guess in list_of_guesses:
+         print("You already tried that letter!")
+       else:
+         print("goodone")
+   
+
 word_list = ['banana','mango','apple','berry','orange'] 
 word_guessed = ['_']
 list_of_guesses = []
 Task1 = Hangman(word_list)
-print(Task1.word)
-print(Task1.word_guessed)
-print(Task1.word_list)
-print(Task1.num_letters)
-print(Task1.list_of_guesses)
+# print(Task1.word_guessed)
+# print(Task1.word_list)
+# print(Task1.num_letters)
+# print(Task1.list_of_guesses)
+print(Task1.check_guess('guess'))
