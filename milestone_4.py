e722410afc3c777b7e6a7e996f2923bf25b6f31f
Roo2409 <roobikapoor@hotmail.com>
2023-01-31 11:52:18 +0000

    this is the final version of task4

--- milestone_4.py
+++ milestone_4.py
@@ -1,38 +1,37 @@
 import random
 class Hangman:
- def __init__(self,word_list,num_lives = 5):
-    self.word_list = word_list
-    self.num_lives = num_lives
-    self.word = random.choice(self.word_list)
-    self.word_guessed = ["_"]*len(self.word)
-    self.num_letters = len(set(self.word))
-    self.list_of_guesses = [ ]
-
- def check_guess(self,guess):
-   self.guess = guess
-   guess = guess.lower()
-   if guess in self.word:
-     print("Good guess! {guess} is in the word.")
- def ask_for_input():
-   while True:
-     guess = input("enter a letter")
-     for letter in guess:
-       if len(guess)!= 1 and not guess.isalpha():
-         print("Invalid letter. Please, enter a single alphabetical character.")
-       elif guess in list_of_guesses:
-         print("You already tried that letter!")
-     if len(guess)==1 not in list_of_guesses:
-       print(" ")
-     else:
-        Hangman.check_guess(guess)
+   def __init__(self,word_list,num_lives = 5):
+      self.word_list = word_list
+      self.num_lives = num_lives
+      self.word = random.choice(self.word_list)
+      self.word_guessed = ["_"]*len(self.word)
+      self.num_letters = len(set(self.word))
+      self.list_of_guesses = [ ]
    
+   def check_guess(self, guess):
+      self.guess = guess.lower()
+      self.list_of_guesses.append(guess)
+      if guess in self.word:
+         print("Good guess! {guess} is in the word.".strip() ) #.strip()
+   
+   def ask_for_input(self):
+      while True:
+         guess = input("enter a letter")
+         if len(guess)!= 1 and not guess.isalpha():
+            print("Invalid letter. Please, enter a single alphabetical character.")
+         elif guess in self.list_of_guesses:
+            print("You already tried that letter!")
+         else:
+         #lif ( len(guess)==1 ) and (guess not in list_of_guesses):
+          self.check_guess(guess)
+
 
 word_list = ['banana','mango','apple','berry','orange'] 
 word_guessed = ['_']
-list_of_guesses = []
-Task1 = Hangman(word_list)
+list_of_guesses =  []
+t1 = Hangman(word_list)
 # print(Task1.word_guessed)
 # print(Task1.word_list)
 # print(Task1.num_letters)
 # print(Task1.list_of_guesses)
-Hangman.ask_for_input()
+t1.ask_for_input()
