49a16790fb73c886318652e8631457fc697794f4
Roo2409 <roobikapoor@hotmail.com>
2023-02-01 13:57:30 +0000

    making a new file

---
+++ milestone_5.py
@@ -0,0 +1,60 @@
+import random
+class Hangman:
+   def __init__(self,word_list,num_lives = 5):
+      self.word_list = word_list
+      self.num_lives = num_lives
+      self.word =  random.choice(self.word_list)   
+      self.word_guessed = ["_"]*len(self.word)
+      self.num_letters = len(set(self.word))
+      self.list_of_guesses = [ ]
+   
+   def check_guess(self,guess):
+      self.guess = guess.lower()
+      self.list_of_guesses.append(guess)
+      if guess in self.word:
+         print(f"Good guess! {guess} is in the word.")
+         self.num_letters-=1  
+         
+         index = 0
+         for letter in self.word:  #apple        
+            if letter == guess:
+            #Find index   
+               self.word_guessed[ index ] = guess
+               index += 1
+            print (f"The word list is {self.word_guessed}")  #explained by hasan!!
+      else:
+         self.num_lives-=1
+
+         print(f"Sorry, {guess} is not in the word.")
+         print(f"You have {self.num_lives} lives left.")
+          
+   def ask_for_input(self):
+      while True:
+         guess = input("enter a letter")
+         if len(guess)!= 1 and not guess.isalpha():
+            print("Invalid letter. Please, enter a single alphabetical character.")
+         elif guess in self.list_of_guesses:
+            print("You already tried that letter!")
+         else:
+         #lif ( len(guess)==1 ) and (guess not in list_of_guesses):
+          self.check_guess(guess)
+          break
+def play_game(self, word_list):
+      num_lives = 5
+      word_list = ['banana','mango','apple','orange'] 
+      word_guessed = ['_']
+      list_of_guesses = []
+      game = Hangman(word_list, num_lives)
+      print(game.word_guessed)
+      print(game.word_list)
+      print(game.list_of_guesses)
+      game.ask_for_input()
+      while True:
+         if num_lives == 0:
+            print("you lost")
+         elif self.num_letters >0:
+            return game.ask_for_input()
+         else:
+            print('Congratulations. You won the game!')
+
+play_game("apple","fruits")
