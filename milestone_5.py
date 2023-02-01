eec7d7f5ea40d3c461291d7053226ae439a90e16
Roo2409 <roobikapoor@hotmail.com>
2023-02-01 17:04:44 +0000

    this is the final edition

--- milestone_5.py
+++ milestone_5.py
@@ -5,7 +5,7 @@ class Hangman:
       self.num_lives = num_lives
       self.word = random.choice(self.word_list)   
       self.word_guessed = ["_"]*len(self.word)
-      self.num_letters = len(set(self.word))
+      self.num_letters = len(self.word)
       self.list_of_guesses = [ ]
    
    def check_guess(self,guess):
@@ -13,19 +13,17 @@ class Hangman:
       self.list_of_guesses.append(guess)
       if guess in self.word:
          print(f"Good guess! {guess} is in the word.")
-      self.num_letters-=1  
-      print(self.num_letters)
-      index = 0
-      for letter in self.word:  #apple        
-            if letter == guess:
-            #Find index   
-               self.word_guessed[ index ] = guess
-               index += 1
-               print (f"The word list is {self.word_guessed}")  #explained by hasan!!
+
+         for letter in range(len(self.word)):     
+               if self.word[letter] == guess:
+                  self.word_guessed[letter]=guess
+                  self.num_letters-=1  
+                  print(self.num_letters)
+         print (f"The word list is {self.word_guessed}")
       else:
          self.num_lives-=1
-      print(f"Sorry, {guess} is not in the word.")
-      print(f"You have {self.num_lives} lives left.")
+         print(f"Sorry, {guess} is not in the word.")
+         print(f"You have {self.num_lives} lives left.")
                
    def ask_for_input(self):
       while True:
@@ -37,12 +35,12 @@ class Hangman:
          else:
           self.check_guess(guess)
           break
+
 def play_game(word_list):
       num_lives = 5
-      word_list = ['banana','mango','apple','orange'] 
       game = Hangman(word_list, num_lives)
       print(game.word_guessed)
-      #print(game.word_list)
+      print(game.word_list)
       print(game.word)
       print(game.list_of_guesses)
       #game.ask_for_input()
@@ -50,9 +48,11 @@ def play_game(word_list):
          if game.num_lives == 0:
             print("you lost")
             break
-         elif game.num_letters >=0:
+         elif game.num_letters >0:
             game.ask_for_input()
          else:
             print('Congratulations. You won the game!')
             break
-play_game("word")
+
+word_list = ['banana','mango','apple','orange'] 
+play_game(word_list)
