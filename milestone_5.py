3f1e0bdd55f05cc20d246fe124a494747245a43e
Roo2409 <roobikapoor@hotmail.com>
2023-02-01 16:05:54 +0000

    test

--- milestone_5.py
+++ milestone_5.py
@@ -3,7 +3,7 @@ class Hangman:
    def __init__(self,word_list,num_lives = 5):
       self.word_list = word_list
       self.num_lives = num_lives
-      self.word =  random.choice(self.word_list)   
+      self.word = "apple" #random.choice(self.word_list)   
       self.word_guessed = ["_"]*len(self.word)
       self.num_letters = len(set(self.word))
       self.list_of_guesses = [ ]
@@ -13,10 +13,10 @@ class Hangman:
       self.list_of_guesses.append(guess)
       if guess in self.word:
          print(f"Good guess! {guess} is in the word.")
-         self.num_letters-=1  
-         
-         index = 0
-         for letter in self.word:  #apple        
+      self.num_letters-=1  
+      print(self.num_letters)
+      index = 0
+      for letter in self.word:  #apple        
             if letter == guess:
             #Find index   
                self.word_guessed[ index ] = guess
@@ -24,7 +24,6 @@ class Hangman:
                print (f"The word list is {self.word_guessed}")  #explained by hasan!!
       else:
          self.num_lives-=1
-
          print(f"Sorry, {guess} is not in the word.")
          print(f"You have {self.num_lives} lives left.")
           
@@ -46,14 +45,15 @@ def play_game(word_list):
       print(game.word_list)
       print(game.word)
       print(game.list_of_guesses)
+      #game.ask_for_input()
       while True:
+         game.ask_for_input()
          if game.num_lives == 0:
             print("you lost")
             break
-         elif game.num_letters >0:
-            print(game.ask_for_input())
+         elif game.num_letters >=0:
+            game.ask_for_input()
          else:
             print('Congratulations. You won the game!')
             break
-
 play_game("word")
