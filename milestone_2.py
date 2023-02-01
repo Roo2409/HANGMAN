cb81a8d936861a7678f79a5eaf50675f3a796e62
Roo2409 <roobikapoor@hotmail.com>
2023-02-01 13:48:00 +0000

    this is a lil change

--- milestone_2.py
+++ milestone_2.py
@@ -9,8 +9,8 @@ word_list = ['banana','mango','apple','berry','orange']
 print(word_list)
 
 guess = input("enter a single letter")
-#for letter in guess:
-    #if len(guess)==1 and guess.isalpha():
-       # print("good guess")
-    #else:
-        #print("Oops! That is not a valid input.")
+for letter in guess:
+    if len(guess)==1 and guess.isalpha():
+       print("good guess")
+    else:
+       print("Oops! That is not a valid input.")
